e99ac62c7158319f64db6270e9807a003e31b920
rikuto <rikuto.10141219@gmail.com>
2020-08-15 18:01:21 +0900

    add barba_custom.js

--- js/barba_custom.js
+++ js/barba_custom.js
@@ -1,35 +1,48 @@
-barba.init();
-
-var FadeTransition = Barba.BaseTransition.extend({
-  start: function() {
-
-    Promise
-      .all([this.newContainerLoading, this.fadeOut()])
-      .then(this.fadeIn.bind(this));
-  },
-
-  fadeOut: function() {
-    return $(this.oldContainer).animate({ opacity: 0 }).promise();
-  },
-
-  fadeIn: function() {
-
-    var _this = this;
-    var $el = $(this.newContainer);
-
-    $(this.oldContainer).hide();
-
-    $el.css({
-      visibility : 'visible',
-      opacity : 0
-    });
-
-    $el.animate({ opacity: 1 }, 400, function() {
-      _this.done();
-    });
-  }
+barba.init({
+    transitions: [{
+        // 現在のページを離れる時のフック
+        leave: function (data) {
+            return new Promise(function (resolve, reject) {
+                leaveAnimation(data.current.container);
+                setTimeout(function () {
+                    resolve();
+                }, 800)
+            });
+        },
+       // 次のページを表示する時のフック
+        enter: function (data) {
+            enterAnimation(data.next.container);
+        }
+    }]
 });
-
-Barba.Pjax.getTransition = function() {
-  return FadeTransition;
-};
+// 現在のページを離れる時のアニメーション
+function leaveAnimation(e) {
+    anime.timeline().add({
+        easing: 'easeOutSine',
+        targets: e.querySelector('.section-01'),
+        duration: 500,
+        opacity: [1, 0],
+        translateY: [0, '-15px']
+    }).add({
+        easing: 'easeOutSine',
+        targets: '.mask',
+        duration: 400,
+        translateY: ['100%', 0]
+    }, '-=200');
+}
+// 次のページを表示する時のアニメーション
+function enterAnimation(e) {
+    anime.timeline().add({
+        easing: 'easeInSine',
+        targets: '.mask',
+        duration: 300,
+        delay: 200,
+        translateY: [0, '-100%']
+    }).add({
+        easing: 'easeInSine',
+        targets: e.querySelector('.section-01'),
+        duration: 400,
+        opacity: [0, 1],
+        translateY: ['15px', 0]
+    }, '-=100');
+}
