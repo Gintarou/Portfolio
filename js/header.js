3858fc9112a9991d7b7a79b7968d913cd3078c18
rikuto <rikuto.10141219@gmail.com>
2020-08-15 23:56:37 +0900

    improve rotate toggle

--- js/header.js
+++ js/header.js
@@ -3,13 +3,14 @@ var ham_menu_wrapper = document.getElementById('ham_menu_wrapper');
 
 ham.addEventListener('click', function() {
   ham.classList.toggle('clicked');
-  document.getElementById('home_name').classList.toggle('home_name_rotate');
   ham_menu_wrapper.classList.toggle('clicked');
 
   if( $('#ham_menu_wrapper').hasClass('clicked') ) {
     $('#ham_menu_wrapper').fadeIn();
+    document.getElementById('home_name').classList.add('home_name_rotate');
   } else {
     $('#ham_menu_wrapper').fadeOut();
+    document.getElementById('home_name').classList.remove('home_name_rotate');
   }
 
 });
