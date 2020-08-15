165edc39618b7a73406011b19e107d32323746e8
rikuto <rikuto.10141219@gmail.com>
2020-08-15 20:09:02 +0900

    improve header

--- js/header.js
+++ js/header.js
@@ -1,6 +1,5 @@
 var ham = document.getElementById('ham');
 var ham_menu_wrapper = document.getElementById('ham_menu_wrapper');
-var header = document.querySelector('header');
 
 ham.addEventListener('click', function() {
   ham.classList.toggle('clicked');
