92c3e85c57e37b89a03fb00de1a54f16e5bce37d
rikuto <rikuto.10141219@gmail.com>
2020-08-15 23:49:35 +0900

    add name

--- js/header.js
+++ js/header.js
@@ -3,6 +3,7 @@ var ham_menu_wrapper = document.getElementById('ham_menu_wrapper');
 
 ham.addEventListener('click', function() {
   ham.classList.toggle('clicked');
+  document.getElementById('home_name').classList.toggle('home_name_rotate');
   ham_menu_wrapper.classList.toggle('clicked');
 
   if( $('#ham_menu_wrapper').hasClass('clicked') ) {
