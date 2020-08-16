5963771f47a726fc6fb36f6a01f0c64d58f25039
rikuto <rikuto.10141219@gmail.com>
2020-08-16 14:33:04 +0900

    add ham_link event

--- js/header.js
+++ js/header.js
@@ -1,6 +1,7 @@
 var ham = document.getElementById('ham');
 var ham_menu_wrapper = document.getElementById('ham_menu_wrapper');
 var ham_link = document.getElementsByClassName('ham_link');
+var home_name = document.getElementById('home_name');
 
 ham.addEventListener('click', function() {
   ham.classList.toggle('clicked');
@@ -11,16 +12,16 @@ ham.addEventListener('click', function() {
       ham.classList.remove('clicked');
       ham_menu_wrapper.classList.remove('clicked');
       $('#ham_menu_wrapper').fadeOut();
-      document.getElementById('home_name').classList.remove('home_name_rotate');
+      home_name.classList.remove('home_name_rotate');
     });
   }
 
   if( $('#ham_menu_wrapper').hasClass('clicked') ) {
     $('#ham_menu_wrapper').fadeIn();
-    document.getElementById('home_name').classList.add('home_name_rotate');
+    home_name.classList.add('home_name_rotate');
   } else {
     $('#ham_menu_wrapper').fadeOut();
-    document.getElementById('home_name').classList.remove('home_name_rotate');
+    home_name.classList.remove('home_name_rotate');
   }
 
 });
