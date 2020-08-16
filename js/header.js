6813edb5c55fa22ac53dea66748e645b0371d50c
rikuto <rikuto.10141219@gmail.com>
2020-08-17 02:53:51 +0900

    changed header.js

--- js/header.js
+++ js/header.js
@@ -6,21 +6,25 @@ var home_name = document.getElementById('home_name');
 ham.addEventListener('click', function() {
   ham.classList.toggle('clicked');
   ham_menu_wrapper.classList.toggle('clicked');
-  $('#ham_menu_wrapper').fadeToggle();
+
+  if( $('#ham_menu_wrapper').hasClass('clicked') && $('#home_name').length ) {
+    $('#ham_menu_wrapper').fadeIn();
+    home_name.classList.add('home_name_rotate');
+  } else {
+    $('#ham_menu_wrapper').fadeOut();
+    home_name.classList.remove('home_name_rotate');
+  }
+
+});
+
+for(let i = 0; i < ham_link.length; i++) {
+  ham_link[i].addEventListener('click', function() {
+    ham.classList.remove('clicked');
+    ham_menu_wrapper.classList.remove('clicked');
+    $('#ham_menu_wrapper').fadeOut();
 
     if(　$('#home_name').length　)　{
-      home_name.classList.toggle('home_name_rotate');
+      home_name.classList.remove('home_name_rotate');
     }
-});
-// 
-// for(let i = 0; i < ham_link.length; i++) {
-//   ham_link[i].addEventListener('click', function() {
-//     ham.classList.remove('clicked');
-//     ham_menu_wrapper.classList.remove('clicked');
-//     $('#ham_menu_wrapper').fadeOut();
-//
-//     if(　$('#home_name').length　)　{
-//       home_name.classList.remove('home_name_rotate');
-//     }
-//   });
-// }
+  });
+}
