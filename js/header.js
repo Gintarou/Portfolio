d823b84f57d3647c6b3ecccef941d7b72060b547
rikuto <rikuto.10141219@gmail.com>
2020-08-16 14:28:04 +0900

    add ham_link event

--- js/header.js
+++ js/header.js
@@ -1,9 +1,16 @@
 var ham = document.getElementById('ham');
 var ham_menu_wrapper = document.getElementById('ham_menu_wrapper');
+var ham_link = document.getElementsByClassName('ham_link');
 
 ham.addEventListener('click', function() {
   ham.classList.toggle('clicked');
   ham_menu_wrapper.classList.toggle('clicked');
+
+  for(let i = 0; i < ham_link.length; i++) {
+    ham_link[i].addEventListener('click', function() {
+      ham.classList.remove('clicked');
+    });
+  }
 
   if( $('#ham_menu_wrapper').hasClass('clicked') ) {
     $('#ham_menu_wrapper').fadeIn();
